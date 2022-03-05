4be4fad86e0192fd8c6990ebea160b6994c0182c
Judy <judy.mtz1@gmail.com>
2022-03-05 15:52:52 -0600

    added const fs, inquirer & started inquirer prompt

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer.prompt ([
+    {
+        type: ``,
+        name: ``,
+        message: ``,
+        choices: [``, ``]
+    }
+]).then()
