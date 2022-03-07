c3111bd387a7bb159203ba61b7f9344f29123ebe
Judy <judy.mtz1@gmail.com>
2022-03-06 22:14:05 -0600

    added choices for employee options

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+const promptManagerQuestions = () => {
+    return inquirer.prompt ([
+        {
+            type: `input`,
+            name: `ManagerName`,
+            message: `What is the team manager's name?`
+        },
+        {
+            type: `input`,
+            name: `ManagerId`,
+            message: `What is the manager's employee ID?`
+        },
+        {
+            type: `input`,
+            name: `ManagerEmail`,
+            message: `What is the manager's email address?`
+        },
+        {
+            type: `input`,
+            name: `ManagerNumber`,
+            message: `What is the manager's phone number?`
+        }
+    
+    ]).then(answers) => {}
 
-inquirer.prompt ([
-    {
-        type: `input`,
-        name: `ManagerName`,
-        message: `What is the team manager's name?`
-    },
-    {
-        type: `input`,
-        name: `ManagerId`,
-        message: `What is the manager's employee ID?`
-    },
-    {
-        type: `input`,
-        name: `ManagerEmail`,
-        message: `What is the manager's email address?`
-    },
-    {
-        type: `input`,
-        name: `ManagerNumber`,
-        message: `What is the manager's phone number?`
-    }
-]).then()
+const promptEmployeeOption = () => {
+    return inquirer.prompt ([
+        {
+            type: `list`,
+            name: `EmployeeOption`,
+            message: `Choose an employee to add to your team`,
+            choices: [`Engineer`, `Intern`],
+
+        }
+    ]).then(answers) => 
+};
