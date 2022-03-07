1279d416702312fe482f78869ecf352cfb87e2aa
Judy <judy.mtz1@gmail.com>
2022-03-06 20:51:36 -0600

    added manager questions

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ inquirer.prompt ([
     {
         type: `input`,
         name: `ManagerName`,
-        message: `What is your manager's name?`
+        message: `What is the team manager's name?`
+    },
+    {
+        type: `input`,
+        name: `ManagerId`,
+        message: `What is the manager's employee ID?`
+    },
+    {
+        type: `input`,
+        name: `ManagerEmail`,
+        message: `What is the manager's email address?`
+    },
+    {
+        type: `input`,
+        name: `ManagerNumber`,
+        message: `What is the manager's phone number?`
     }
 ]).then()
